9f613a117106f3edf14238769d31f42130a69b4a
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-03-27 17:05:23 -0500

    Updated app.ts to integrate swagger

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import cors from "cors";
 import { getHelmetConfig } from "../config/helmetConfig";
 import { getCorsOptions } from "../config/corsConfig";
+import setupSwagger from "../config/swagger";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
@@ -25,5 +26,8 @@ app.use(morgan("combined"));
 // Routes
 app.use("/api/v1", healthRoutes);
 app.use("/api/v1/events", eventRoutes);
+
+// Setup Swagger
+setupSwagger(app);
 
 export default app;
