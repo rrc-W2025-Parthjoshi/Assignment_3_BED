674197bdafa6101c5743d91c3b7c20ca1c94b81f
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-03-27 16:09:18 -0500

    Added helmet.js configuration

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,15 @@ dotenv.config();
 
 import express, { Express } from "express";
 import morgan from "morgan";
+import { getHelmetConfig } from "../config/helmetConfig";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
 // Initialize Express application
 const app: Express = express();
+
+// Security Middleware
+app.use(getHelmetConfig());
 
 // Middleware
 app.use(express.json());
